ae27849922ee9a6d85e66c4956383d70ec33f142
Emmad Usmani <emmadausmani@gmail.com>
2021-01-17 10:43:32 -0800

    moved utils from index to utils/game

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,14 +1,1 @@
-import { GameSettings } from "@connect-game/shared";
-
-/* Converts boardSize to str for use in Picker */
-export function boardSizeToStr(boardSize: GameSettings["boardSize"]) {
-  return boardSize.join(",");
-}
-
-export function strToBoardSize(str: string) {
-  return str
-    .split(",")
-    .map((num) => parseInt(num)) as GameSettings["boardSize"];
-}
-
-// TODO: consider moving to static methods of Game class
+export * from "./game";
