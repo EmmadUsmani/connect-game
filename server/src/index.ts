afee111d562372573efcc5fa5343593dfe9f2c64
Emmad Usmani <emmadausmani@gmail.com>
2021-03-26 15:24:42 -0700

    host react app from express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
 import { createServer } from "http";
+import * as path from "path";
+import express from "express";
 import { Server } from "socket.io";
 
 import { initListeners } from "./listeners";
 
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
 
+const buildPath = path.join(__dirname, "..", "..", "client", "build");
+
+app.use(express.static(buildPath));
+
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+
+// allow cors for create-react-app development
 const io = new Server(httpServer, {
   cors: {
     origin: "http://localhost:3000",
