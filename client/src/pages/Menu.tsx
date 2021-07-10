ac1f6367e54f5bd286882e9af42d26929d5eff99
Emmad Usmani <emmadausmani@gmail.com>
2021-07-10 14:09:20 -0700

    implement closing and leaving logic in menu

--- client/src/pages/Menu.tsx
+++ client/src/pages/Menu.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import { Modal } from "../components/layouts";
 import { Button, Toggle } from "../components";
 import { useOptions } from "../context";
 import { useOnKeyDown } from "../hooks";
 
+// TODO: change "leave game" text based on route
+
 const Menu: React.FC = () => {
   const { soundsOn, animationsOn, toggleSounds, toggleAnimations } =
     useOptions();
+  const history = useHistory();
 
   const [showModal, setShowModal] = useState<boolean>(false);
-  useOnKeyDown("Escape", () => setShowModal((showModal) => !showModal));
+  const toggleModal = () => setShowModal((showModal) => !showModal);
+  useOnKeyDown("Escape", toggleModal);
+
+  const handleLeave = (): void => {
+    history.push("/");
+    toggleModal();
+  };
 
   return showModal ? (
     <Modal>
@@ -20,8 +30,10 @@ const Menu: React.FC = () => {
         label="Animations"
         onClick={toggleAnimations}
       />
-      <Button type="negative">Leave Game</Button>
-      <Button>Close</Button>
+      <Button type="negative" onClick={handleLeave}>
+        Leave Game
+      </Button>
+      <Button onClick={toggleModal}>Close</Button>
     </Modal>
   ) : (
     <></>
