d88613517ad0d1829311d5e3ba4398752c85356c
Emmad Usmani <emmadausmani@gmail.com>
2021-07-11 00:08:43 -0700

    configure scripts & dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,40 @@
 import { createServer } from "http";
 import * as path from "path";
+import * as dotenv from "dotenv";
 import express from "express";
 import { Server } from "socket.io";
 
 import { initListeners } from "./listeners";
 
+dotenv.config({ path: path.join(__dirname, "..", "..", ".env") });
 const app = express();
 const httpServer = createServer(app);
 
-const buildPath = path.join(__dirname, "..", "..", "client", "build");
+// serve frontend in prod
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.join(__dirname, "..", "..", "client", "build");
+  app.use(express.static(buildPath));
+  app.get("/*", (_, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
+}
 
-app.use(express.static(buildPath));
-
-app.get("/*", (_, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
-});
-
-// allow cors for create-react-app development
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+// allow cors in dev
+const io = new Server(
+  httpServer,
+  process.env.NODE_ENV === "development"
+    ? {
+        cors: {
+          origin: "http://localhost:3000",
+          methods: ["GET", "POST"],
+        },
+      }
+    : {}
+);
 
 initListeners(io);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT ? +process.env.PORT : 3001;
 
 httpServer.listen(port, () => {
   console.log("Listening on port " + port);
