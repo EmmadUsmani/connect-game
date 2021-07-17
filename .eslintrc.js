8699495142824ee9d035f0f3208d7570200275a4
Emmad Usmani <emmadausmani@gmail.com>
2021-07-17 12:26:53 -0700

    configure eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,25 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "prettier",
   ],
+  rules: {
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/extensions": ["error", "never"],
+    "import/no-default-export": "error",
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
 };
