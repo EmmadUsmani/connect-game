ad5d74b1e6b24284dc6b28d61d4ffa798a4c4b89
Emmad Usmani <emmadausmani@gmail.com>
2021-07-24 18:04:55 -0700

    add tooltip to MenuIcon

--- client/src/modules/menu/components/MenuIcon.tsx
+++ client/src/modules/menu/components/MenuIcon.tsx
@@ -1,6 +1,9 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import ReactTooltip from "react-tooltip"
 import styled from "styled-components"
+
+import { useTheme } from "context"
 
 interface MenuIconProps {
   onClick(): void
@@ -14,9 +17,22 @@ const StyledIconDiv = styled.div`
 `
 
 export function MenuIcon({ onClick }: MenuIconProps) {
+  const theme = useTheme()
+
   return (
-    <StyledIconDiv onClick={onClick}>
-      <FontAwesomeIcon icon={faBars} size="2x" />
-    </StyledIconDiv>
+    <>
+      <StyledIconDiv
+        data-arrow-color="rgba(0, 0, 0, 0)"
+        data-background-color={theme.colors.primary}
+        data-effect="solid"
+        data-for="menu"
+        data-place="left"
+        data-tip="Escape"
+        onClick={onClick}
+      >
+        <FontAwesomeIcon icon={faBars} size="2x" />
+      </StyledIconDiv>
+      <ReactTooltip id="menu" />
+    </>
   )
 }
