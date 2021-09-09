0099ba638e5b05726329c56190b278f8adb64101
Emmad Usmani <emmadausmani@gmail.com>
2021-09-09 08:50:37 -0700

    add kbd element and style to MenuIcon "Escape" text

--- client/src/modules/menu/components/MenuIcon.tsx
+++ client/src/modules/menu/components/MenuIcon.tsx
@@ -16,6 +16,24 @@ const StyledDiv = styled.div`
   right: 20px;
 `
 
+/* Style taken from MDN Web Docs
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/kbd */
+
+const StyledKbd = styled.kbd`
+  background-color: #eee;
+  border-radius: 3px;
+  border: 1px solid #b4b4b4;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2),
+    0 2px 0 0 rgba(255, 255, 255, 0.7) inset;
+  color: #333;
+  display: inline-block;
+  font-size: 0.85em;
+  font-weight: 700;
+  line-height: 1;
+  padding: 2px 4px;
+  white-space: nowrap;
+`
+
 export function MenuIcon({ onClick }: MenuIconProps) {
   const theme = useTheme()
 
@@ -30,7 +48,7 @@ export function MenuIcon({ onClick }: MenuIconProps) {
         arrowColor="rgba(0, 0, 0, 0)"
         backgroundColor={theme.colors.primary}
         effect="solid"
-        getContent={() => "Escape"}
+        getContent={() => <StyledKbd>Esc</StyledKbd>}
         id="menu"
         place="left"
       />
